91c26ec0cdaa9247471136b9e4568ee39c8213e8
AlexKolokolov <kolokolov_a@ukr.net>
2017-03-20 16:46:38 +0200

    #fat-836 add logging to Utils object
    Add logging using Slf4j to Utils
    object

--- src/main/scala/org/kolokolov/Utils.scala
+++ src/main/scala/org/kolokolov/Utils.scala
@@ -1,15 +1,20 @@
 package org.kolokolov
 
+import org.slf4j.LoggerFactory
+
 /**
   * Created by Alexey Kolokolov on 20.03.2017.
   */
 object Utils {
+
+  val logger = LoggerFactory.getLogger("Utils logger")
 
   def main(args: Array[String]): Unit = {
     println(sum(Some(1), Some(2), Some(3)))
   }
 
   def sum(x: Option[Int], y: Option[Int], z: Option[Int]): Option[Int] = {
+    logger.info("Method sum is running")
     for {
       _x <- x
       _y <- y
