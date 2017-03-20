f814da035106c39d19a08c80be992d755cc85bb5
kolokolov <kolokolov_a@ukr.net>
2017-03-20 22:28:10 +0200

    #fat-836 add one more test to UtilsTest

--- src/test/scala/org/kolokolov/UtilsTest.scala
+++ src/test/scala/org/kolokolov/UtilsTest.scala
@@ -5,9 +5,13 @@ import org.scalatest.FlatSpec
 /**
   * Created by Alexey Kolokolov on 20.03.2017.
   */
-class UtilsTest extends FlatSpec{
+class UtilsTest extends FlatSpec {
 
   "Utils.sum" should "return Some(6)" in {
-    assert(Utils.sum(Some(1), Some(2), Some(3)) == Some(6))
+    assert(Utils.sum(Some(1), Some(2), Some(3)).contains(6))
+  }
+
+  it should "return None" in {
+    assert(Utils.sum(Some(1), Some(2), None).isEmpty)
   }
 }
