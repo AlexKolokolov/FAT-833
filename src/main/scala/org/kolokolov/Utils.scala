3e1cf50a5fc5d1e199bf9fe9b45f50b26f6d51d2
AlexKolokolov <kolokolov_a@ukr.net>
2017-03-20 16:26:07 +0200

    #fat-836 add main method to Utils.scala
    Add main method to Utils.scala for
    testing purposes

--- src/main/scala/org/kolokolov/Utils.scala
+++ src/main/scala/org/kolokolov/Utils.scala
@@ -4,6 +4,11 @@ package org.kolokolov
   * Created by Alexey Kolokolov on 20.03.2017.
   */
 object Utils {
+
+  def main(args: Array[String]): Unit = {
+    println(sum(Some(1), Some(2), Some(3)))
+  }
+
   def sum(x: Option[Int], y: Option[Int], z: Option[Int]): Option[Int] = {
     for {
       _x <- x
