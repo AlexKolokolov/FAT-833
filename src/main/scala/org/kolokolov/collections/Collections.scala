9c154256c157cef2432893efbf1764de6f45514c
AlexKolokolov <kolokolov_a@ukr.net>
2017-03-21 16:31:35 +0200

    Remove main method from Collections

--- src/main/scala/org/kolokolov/collections/Collections.scala
+++ src/main/scala/org/kolokolov/collections/Collections.scala
@@ -30,9 +30,4 @@ object Collections {
   def sortTupleList(tupleList: List[(Int,String)]): List[(Int,String)] = {
     tupleList.view.map(_._1).sorted.toList zip tupleList.view.map(_._2).sorted.reverse.toList
   }
-
-  def main(args: Array[String]): Unit = {
-    val list = List((8, "aaa"),(1, "bbb"),(5,"zzz"),(11,"fff"))
-    print(sortTupleList(list))
-  }
 }
