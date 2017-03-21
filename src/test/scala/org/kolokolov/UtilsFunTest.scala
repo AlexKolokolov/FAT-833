9915d6dba2f4f48cfe56246eced4e85be0bac6f0
AlexKolokolov <kolokolov_a@ukr.net>
2017-03-21 11:45:07 +0200

    #fat-836 add individual config for tests logging
    Add individual config file logback-test.xml to
    test/resources/ for configuring logging in tests

--- src/test/scala/org/kolokolov/UtilsFunTest.scala
+++ src/test/scala/org/kolokolov/UtilsFunTest.scala
@@ -1,11 +1,16 @@
 package org.kolokolov
 
 import org.scalatest.FunSuite
+import org.slf4j.LoggerFactory
 
 /**
   * Created by andersen on 21.03.2017.
   */
 class UtilsFunTest extends FunSuite {
+
+  private val logger = LoggerFactory.getLogger("UtilsFunTest logger")
+
+  logger.info("UtilsFunTest is running")
 
   test("Utils.sum returns Some(6)") {
     assert(Utils.sum(Some(1), Some(2), Some(3)).contains(6))
