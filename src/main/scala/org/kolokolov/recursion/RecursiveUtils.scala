ca6406d4e0b65a6e20ddebe4edf1a3cc48ef70e4
AlexKolokolov <kolokolov_a@ukr.net>
2017-03-23 13:16:27 +0200

    #fat-852 add splitByNegative to RecursiveUtils
    Add splitByNegative method to RecursiveUtils
    with test

--- src/main/scala/org/kolokolov/recursion/RecursiveUtils.scala
+++ src/main/scala/org/kolokolov/recursion/RecursiveUtils.scala
@@ -9,7 +9,7 @@ object RecursiveUtils {
 
   def findMax(xs: List[Int]): Int = {
     @tailrec def findMaxRec(xs: List[Int], max: Int): Int = {
-      if (xs == Nil) max
+      if (xs.isEmpty) max
       else if (xs.head > max) findMaxRec(xs.tail, xs.head)
       else findMaxRec(xs.tail, max)
     }
@@ -21,9 +21,18 @@ object RecursiveUtils {
       def lessAndGreater(a: Int, b: Int): (Int,Int) = {
         if (a < b) (a,b) else (b,a)
       }
-      if (xs == Nil) (min,max)
+      if (xs.isEmpty) (min,max)
       else findMinMaxRec(xs.tail, lessAndGreater(xs.head, min)._1, lessAndGreater(xs.head, max)._2)
     }
     findMinMaxRec(xs, xs.head, xs.head)
+  }
+
+  def splitByNegative(xs: List[Int]): (List[Int],List[Int]) = {
+    @tailrec def splitRec(xs: List[Int], neg: List[Int], pos: List[Int]): (List[Int],List[Int]) = {
+      if (xs.isEmpty) (neg,pos)
+      else if (xs.head >= 0) splitRec(xs.tail, neg, pos :+ xs.head)
+      else splitRec(xs.tail, neg :+ xs.head, pos)
+    }
+    splitRec(xs, Nil, Nil)
   }
 }
--- src/test/scala/org/kolokolov/recursion/RecursiveUtilsTest.scala
+++ src/test/scala/org/kolokolov/recursion/RecursiveUtilsTest.scala
@@ -7,15 +7,21 @@ import org.scalatest.FunSuite
   */
 class RecursiveUtilsTest extends FunSuite {
 
-  test {"findMax should return 13"} {
+  test ("findMax should return 13") {
     val testList = List(1, 5, 10, -1, 2, 13, 9, 11)
     val expectedResult = 13
     assert(RecursiveUtils.findMax(testList) == expectedResult)
   }
 
-  test {"findMinMax should return (13, -1)"} {
+  test ("findMinMax should return (13, -1)") {
     val testList = List(1, 5, 10, -1, 2, 13, 9, 11)
     val expectedResult = (-1, 13)
     assert(RecursiveUtils.findMinMax(testList) == expectedResult)
+  }
+
+  test ("splitByNegative should return (List(-1, -4),List(0, 5, 3))") {
+    val testList = List(0, 5, -1, 3, -4)
+    val expectedResult = (List(-1, -4),List(0, 5, 3))
+    assert(RecursiveUtils.splitByNegative(testList) == expectedResult)
   }
 }
