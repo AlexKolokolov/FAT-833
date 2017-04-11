08612e162c49b48c2d071a97d816adc30eaa6909
AlexKolokolov <kolokolov_a@ukr.net>
2017-04-11 10:33:37 +0300

    #fat-864 remake unit tests for REST controllers
    Remake unit tests for UserController and
    GroupController using AsyncFunSuiteLike
    trait

--- src/test/scala/org/kolokolov/scalatra/unit/UserControllerTest.scala
+++ src/test/scala/org/kolokolov/scalatra/unit/UserControllerTest.scala
@@ -3,10 +3,10 @@ package org.kolokolov.scalatra.unit
 import _root_.akka.actor.ActorSystem
 import org.kolokolov.scalatra.controller.UserController
 import org.kolokolov.slick.DBprofiles.H2Database
-import org.kolokolov.slick.model.User
-import org.kolokolov.slick.service.UserService
+import org.kolokolov.slick.model.{Group, User}
+import org.kolokolov.slick.service.{UserGroupService, UserService}
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{BeforeAndAfterEach, FunSuiteLike}
+import org.scalatest.AsyncFunSuiteLike
 import org.scalatra.test.scalatest.ScalatraSuite
 import org.slf4j.LoggerFactory
 
@@ -18,8 +18,7 @@ import scala.concurrent.Future
   */
 class UserControllerTest extends ScalatraSuite
   with MockFactory
-  with FunSuiteLike
-  with BeforeAndAfterEach {
+  with AsyncFunSuiteLike {
 
   private val logger = LoggerFactory.getLogger(this.getClass)
 
@@ -27,13 +26,18 @@ class UserControllerTest extends ScalatraSuite
 
   val firstUser = User("Bob Marley",1)
   val secondUser = User("Tom Waits",2)
+  val group = Group("Admin",1)
 
-  val userServiceStub: UserService = stub[UserService]
+  private val userServiceStub = stub[UserService]
   (userServiceStub.getUserById _).when(firstUser.id).returns(Future(Some(firstUser)))
   (userServiceStub.getAllUsers _).when().returns(Future(Seq(firstUser,secondUser)))
 
+  private val userGroupServiceStub = stub[UserGroupService]
+  (userGroupServiceStub.getUsersByGroupId _).when(group.id).returns(Future(Seq((firstUser,group))))
+
   class MockedUserController extends UserController(system) with H2Database {
     override protected lazy val userService: UserService = userServiceStub
+    override protected lazy val userGroupService: UserGroupService = userGroupServiceStub
   }
 
   addServlet(new MockedUserController, "/users")
@@ -44,9 +48,16 @@ class UserControllerTest extends ScalatraSuite
     }
   }
 
-  ignore("GET /users/1 should return {name:Bob Marley,id:1}") {
+  test("GET /users/1 should return {name:Bob Marley,id:1}") {
     logger.debug("get /users/1 test is running")
     get("/users/1") {
+      body should include (firstUser.name)
+    }
+  }
+
+  test("GET /users/group/1 should return [{_1:{name:Bob Marley,id:1},_2:{name:Admin,id:1}}]") {
+    logger.debug("get /users/group/1 test is running")
+    get("/users/group/1") {
       body should include (firstUser.name)
     }
   }
